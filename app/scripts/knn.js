065e9dd4cda0470264a4bc0c18e5b8bf5925c90a
Casper Clemmensen <cascle01@gmail.com>
2016-04-17 22:47:24 +0200

    Yes

--- app/scripts/knn.js
+++ app/scripts/knn.js
@@ -6,12 +6,13 @@ class KNN {
     this.districts = undefined;
     this.cvsDestination = 'datasets/pb/PASSENGER_VEHICLE_CLUSTERS.csv';
 
-    this.height = 530;
+    this.height = 730;
     this.width = 530;
     this.offset = [this.width / 2, this.height / 2]
     this.scale = 200000;
-    this.projection = undefined;
 
+    this.projection = undefined;
+    this.path = undefined;
     this.center = undefined;
 
     elm.style.height = `${this.height}px`;
@@ -20,55 +21,49 @@ class KNN {
     //d3.cvs('datasets/pb/PASSENGER_VEHICLE_CLUSTERS.csv', this.)
     d3.json('datasets/pb/School_Districts.geojson', json => this.onDistrictsLoaded(json));
   }
-  onDistrictsLoaded(districts) {
-    this.districts = districts;
-    this.center = d3.geo.centroid(districts);
-  //
-  //   this.renderDistricts();
-  // }
-  // renderDistricts() {
+  onDistrictsLoaded(json) {
+    this.districts = json;
+    requestAnimationFrame(() => this.renderSectors());
+  }
+  renderSectors() {
 
-    // create the path
-
+    this.center = d3.geo.centroid(this.districts)
     this.projection = d3.geo.mercator().scale(this.scale).center(this.center).translate(this.offset);
 
-    const path = d3.geo.path().projection(this.projection);
+    // create the path
+    this.path = d3.geo.path().projection(this.projection);
 
-
-
-    const bounds = path.bounds(this.district);
-    const hscale = this.scale * this.width / (bounds[1][0] - bounds[0][0]);
-    const vscale = this.scale * this.height / (bounds[1][1] - bounds[0][1]);
+    // using the path determine the bounds of the current map and use
+    // these to determine better values for the scale and translation
+    let bounds = this.path.bounds(this.districts);
+    let hscale = this.scale * this.width / (bounds[1][0] - bounds[0][0]);
+    let vscale = this.scale * this.height / (bounds[1][1] - bounds[0][1]);
     this.scale = (hscale < vscale) ? hscale : vscale;
-    this.offset = [
-      this.width - (bounds[0][0] + bounds[1][0]) / 2,
+    this.offset = [this.width - (bounds[0][0] + bounds[1][0]) / 2,
       this.height - (bounds[0][1] + bounds[1][1]) / 2
     ];
-    // draw(this.svg, path, this.districts)
 
-    //Render the districts
-    this.svg.selectAll("path")
-      .data(districts)
-      .enter()
-      .append("path")
-      .attr({
-        "d": path,
-        'fill': 'rgb(129,145,152)',
-        'stroke-width': 2,
-        'stroke': "white"
-      })
+    // new projection
+    this.projection = d3.geo.mercator().scale(this.scale).center(this.center).translate(this.offset);
+    this.path = this.path.projection(this.projection);
+
+    if (requestIdleCallback) {
+      requestIdleCallback( () => this.renderD3Elems());
+    } else {
+      this.renderD3Elems();
+    }
   }
-
-}
-function draw(svg, path, dis) {
-  svg.append('g').selectAll("path")
-    .data(dis)
+  renderD3Elems() {
+    this.svg.selectAll("path")
+    .data(this.districts.features)
     .enter()
     .append("path")
     .attr({
-      "d": path,
-      'fill': 'rgb(129,145,152)',
-      'stroke-width': 2,
+      "d": this.path,
+      'fill': 'grey',
+      'stroke-width': 1,
       'stroke': "white"
     })
+
+  }
 }
