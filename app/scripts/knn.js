4553163a33cb5863e81e5f40e0172fbf5b5e515c
Casper Clemmensen <cascle01@gmail.com>
2016-04-23 16:56:42 +0200

    Chagned to topo json

--- app/scripts/knn.js
+++ app/scripts/knn.js
@@ -9,43 +9,32 @@ class KNN {
     this.height = 730;
     this.width = 530;
     this.offset = [this.width / 2, this.height / 2]
-    this.scale = 200000;
+    this.scale = 75000;
 
     this.projection = undefined;
     this.path = undefined;
-    this.center = undefined;
+    this.center = [-74.0130, 40.7238];
 
     elm.style.height = `${this.height}px`;
     elm.style.width = '100%';
 
-    //d3.cvs('datasets/pb/PASSENGER_VEHICLE_CLUSTERS.csv', this.)
-    d3.json('datasets/pb/School_Districts.geojson', json => this.onDistrictsLoaded(json));
+    d3.json('datasets/pb/zip_districts.topojson', json => this.onDistrictsLoaded(json));
+   
   }
   onDistrictsLoaded(json) {
     this.districts = json;
+    this.topo = topojson.feature(json, json.objects.zip_districts);
     requestAnimationFrame(() => this.renderSectors());
   }
   renderSectors() {
 
-    this.center = d3.geo.centroid(this.districts)
-    this.projection = d3.geo.mercator().scale(this.scale).center(this.center).translate(this.offset);
+    this.projection = d3.geo.mercator()
+      .scale(this.scale)
+      .center(this.center)
+      .translate([this.width / 2, this.height / 2])
 
     // create the path
     this.path = d3.geo.path().projection(this.projection);
-
-    // using the path determine the bounds of the current map and use
-    // these to determine better values for the scale and translation
-    let bounds = this.path.bounds(this.districts);
-    let hscale = this.scale * this.width / (bounds[1][0] - bounds[0][0]);
-    let vscale = this.scale * this.height / (bounds[1][1] - bounds[0][1]);
-    this.scale = (hscale < vscale) ? hscale : vscale;
-    this.offset = [this.width - (bounds[0][0] + bounds[1][0]) / 2,
-      this.height - (bounds[0][1] + bounds[1][1]) / 2
-    ];
-
-    // new projection
-    this.projection = d3.geo.mercator().scale(this.scale).center(this.center).translate(this.offset);
-    this.path = this.path.projection(this.projection);
 
     if (requestIdleCallback) {
       requestIdleCallback( () => this.renderD3Elems());
@@ -54,16 +43,17 @@ class KNN {
     }
   }
   renderD3Elems() {
-    this.svg.append('g').selectAll("path")
-    .data(this.districts.features)
-    .enter()
-    .append("path")
-    .attr({
-      "d": this.path,
-      'fill': 'grey',
-      'stroke-width': 1,
-      'stroke': "white"
-    })
-
+    this.svg
+        .append('g')
+        .selectAll('path')
+        .data(this.topo.features)
+        .enter()
+        .append('path')
+        .attr({
+          "d": d3.geo.path().projection(this.projection),
+          'fill': 'grey',
+          'stroke-width': 1,
+          'stroke': "white"
+        });
   }
 }
