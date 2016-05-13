780c970eae53037a8efe5f3c50cbbab5f8235f6f
Casper <cascle01@gmail.com>
2016-05-13 09:29:07 +0200

    Fixed the Dropdown, and cleaned up the code

--- app/scripts/BarChartOffset.js
+++ app/scripts/BarChartOffset.js
@@ -32,18 +32,24 @@ class BarChartOffset {
         this.tooltip = opt.tooltip;
         if (opt.tooltip) this.tooltip.elm = d3.select(opt.tooltip.id);
 
-        this.reference = opt.reference || 'Mostly Cloudy';
+        this.dropdown = opt.dropdown;
+        if (opt.dropdown) this.dropdown.elm = d3.select(opt.dropdown.selector);
+        if (opt.dropdown && opt.dropdown.default) this.dropdown.value = opt.dropdown.default;
+
 
         this.cutAt = opt.cutAt;
         this.getValue = opt.value || _.noop;
         this.getLabel = opt.labels || ((e, i) => i)
         this.data = null;
+        this.rawData = null;
 
         this.bars = this.svg.append('g');
         this.yAxis = this.svg.append('g');
         this.xAxis = this.svg.append('g');
         this.line = opt.line;
         this.onClick = opt.onClick || _.noop;
+
+        this.categoryChanged = this.categoryChanged.bind(this);
 
         svg.style.height = this.height + 30;
         svg.style['padding-left'] = this.paddingLeft;
@@ -62,11 +68,18 @@ class BarChartOffset {
             );
         }
     }
+    findPeaks() {
+        const max = d3.max(_.values(this.rawData));
+        const min = d3.min(_.values(this.rawData));
+
+        return [-((max / min * 100) - 100), (max / min * 100) - 100]
+    }
     onDataLoaded(data) {
+        this.rawData = data;
         if (this.cutAt) {
-            this.data = this.normalize(data.slice(0, this.cutAt), this.reference);
+            this.data = this.normalize(data.slice(0, this.cutAt), this.dropdown.value);
         } else {
-            this.data = this.normalize(data, this.reference);
+            this.data = this.normalize(data, this.dropdown.value);
         }
 
         // X scale
@@ -74,7 +87,7 @@ class BarChartOffset {
 
         // Y scale
         this.yScale = this.yScale
-            .domain([0, d3.max(d3.values(this.data).map(this.getValue))])
+            .domain(this.findPeaks())
             .range([0, this.width - (2 * this.paddingLeft) - 20]);
 
 
@@ -89,32 +102,28 @@ class BarChartOffset {
 
                 this.renderBars();
                 this.renderAxis();
-                if (this.line) this.renderLine();
-
+                this.renderLine();
+                if (this.dropdown) this.renderDropdown();
             });
         } else {
             if (this.hasRendered) return;
 
             this.renderBars();
             this.renderAxis();
-            if (this.line) this.renderLine();
+            this.renderLine();
+            if (this.dropdown) this.renderDropdown();
         }
 
     }
     normalize(data, feature) {
-      // reference = ratios["Mostly Cloudy"]
-      // for k in ratios:
-      //   ratios[k] = (ratios[k]/reference)*100
-      
-      const reference = this.data[feature]
-      return _.mapValues(this.data, d => (d/reference*100) - 100)
+
+        const reference = data[feature]
+        return _.mapValues(data, d => (d / reference * 100) - 100)
     }
     renderBars() {
         this.hasRendered = true;
 
         const max = d3.max(d3.values(this.data).map(this.getValue));
-        const size = _.size(this.data);
-
 
         this.bars
             .selectAll('rect')
@@ -122,7 +131,7 @@ class BarChartOffset {
             .enter()
             .append('rect')
             .attr({
-                'x': (d, i) => (this.paddingLeft),
+                'x': (d, i) => this.yScale(0) + this.paddingLeft,
                 'height': this.xScale.rangeBand(),
                 'fill': 'cornflowerblue',
                 'y': (d, i) => (this.xScale(i) + this.paddingTop),
@@ -130,15 +139,28 @@ class BarChartOffset {
             })
             .on('mouseover', this.renderTooltip.bind(this))
             .on('mouseout', this.hideTooltip.bind(this))
-            .on('click', this.onClick)
+            .on('click', this.onClick);
+
+        this.renderBarTransition();
+    }
+    renderBarTransition() {
+        const size = _.size(this.data);
+
+        this.bars
+            .selectAll('rect')
             .transition()
             .duration(2000)
             .delay((d, i) => ((i / size * 1000)))
             .attr({
-                'width': d => (Math.abs(this.yScale(0) - this.yScale(this.getValue(d))))
+                'width': d => (Math.abs(this.yScale(0) - this.yScale(this.getValue(d)))),
+                'x': d => {
+                    const y = d >= 0 ? 0 : d;
+                    return this.yScale(y) + this.paddingLeft;
+                }
             })
     }
     renderAxis() {
+
         // Define axes
         let xAxis = d3.svg.axis().scale(this.xScale).orient('left').tickFormat((d) => this.xLabels[d]);
         let yAxis = d3.svg.axis().scale(this.yScale).orient('bottom').ticks(this.ticks);
@@ -162,23 +184,22 @@ class BarChartOffset {
             });
     }
     renderLine() {
-        const x = this.line.x(this.data);
+        const x = _.size(this.data) - 1;
 
         this.svg.append('line')
             .attr({
-                'y1': this.yScale.range()[1] - this.paddingTop,
-                'x1': this.paddingLeft + (Math.abs(this.yScale(0) - this.yScale(x))),
-                'y2': this.yScale.range()[1] - this.paddingTop,
-                'x2': this.paddingLeft + (Math.abs(this.yScale(0) - this.yScale(x))),
+                'y1': this.xScale(x) + this.xScale(1) + this.paddingTop,
+                'x1': this.paddingLeft + this.yScale(0),
+                'y2': this.xScale(x) + this.xScale(1) + this.paddingTop,
+                'x2': this.paddingLeft + this.yScale(0),
                 'stroke': 'black',
                 'stroke-width': '1',
-                'stroke-dasharray': '10, 10',
                 'shape-rendering': 'crispEdges'
             })
             .transition()
             .duration(2000)
             .attr({
-                'y1': this.yScale.range()[0] + this.paddingTop,
+                'y1': this.xScale(0) + this.paddingTop
             })
     }
     renderTooltip(elm, i) {
@@ -192,7 +213,7 @@ class BarChartOffset {
             .style('opacity', 1)
             .duration(100);
 
-        this.tooltip.render(this.tooltip.elm, elm, this.xLabels[i], i);
+        this.tooltip.render(this.tooltip.elm, elm, this.xLabels[i], i, (this.dropdown) ? this.dropdown.value : undefined);
 
     }
     hideTooltip() {
@@ -202,5 +223,27 @@ class BarChartOffset {
             .transition()
             .duration(150)
             .style('opacity', 0);
+    }
+    renderDropdown() {
+        this.dropdown.elm
+            .selectAll('option')
+            .data(d3.keys(this.data))
+            .enter()
+            .append('option')
+            .attr({
+                'value': (d => d)
+            })
+            .property('selected', d => (d === this.dropdown.value))
+            .text(d => d);
+
+        this.dropdown.elm
+            .on('change', this.categoryChanged)
+    }
+    categoryChanged(e) {
+        this.dropdown.value = this.dropdown.elm.node().value;
+        this.data = this.normalize(this.rawData, this.dropdown.value);
+
+        this.renderAxis();
+        this.renderBars();
     }
 }
